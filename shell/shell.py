299bab23854efb133978f041c3192ed9332a71ed
Efrain Retana <eretana1@miners.utep.edu>
2022-02-17 00:19:06 -0700

    Complete CD command, update redirection. Change form of input to utilize OS.read instead of input()

--- shell/shell.py
+++ shell/shell.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import os, sys, re
 
 
@@ -23,31 +25,38 @@ pid = os.getpid()  # Parent PID
 args = ''
 while True:
     # Read command from user
-    args = input(f'{sys.ps1}').split(" ")
+    os.write(1, (f'~{os.getcwd()}: {sys.ps1}').encode())
+    args = os.read(0,1000).decode().split()
 
     # Check if user wants to exit
     if args[0] == 'exit':
         sys.exit(1)
-            
-    # Fork to create child for read command
-    os.write(2, ("Fork with PID: %d\n" % pid).encode())
-    rc = os.fork()
 
-    if rc < 0:  # Fork has failed
-        os.write(2, ("err: Failed to fork with PID: %d\n" % pid).encode())
-        sys.exit(1)
-    elif rc == 0:  # Child in progress
-        if '>' in args or '>>' in args:
-            file_index = search_redirect_file(args)
-            os.close(1)  # redirect child's stdout
-            os.open(args[file_index], os.O_CREAT | os.O_WRONLY);
-            os.set_inheritable(1, True)
-            args = args[:(file_index - 1)]
-
-        if args[0] == 'cd':
-            os.write(2, 'Cd command in progress\n'.encode())
+    # Change current directory
+    if args[0] == 'cd':
+        if len(args) == 1:
+            os.chdir('/')
         else:
+            try:
+                os.chdir(f'{args[1]}')
+            except:
+                os.write(1, ('Invalid directory: %s\n' % args[1]).encode())
+    else:
+        # Fork to create child for read command
+        os.write(2, ("Fork with PID: %d\n" % pid).encode())
+        rc = os.fork()
 
+        if rc < 0:  # Fork has failed
+            os.write(2, ("err: Failed to fork with PID: %d\n" % pid).encode())
+            sys.exit(1)
+        elif rc == 0:  # Child in progress
+            if '>' in args or '>>' in args:
+                file_index = search_redirect_file(args)
+                os.close(1)  # redirect child's stdout
+                os.open(args[file_index], os.O_CREAT | os.O_WRONLY);
+                os.set_inheritable(1, True)
+                args = args[:(file_index - 1)]
+                
             for directory in re.split(":", os.environ['PATH']):  # try each directory in the path
                 program = "%s/%s" % (directory, args[0])
                 try:
@@ -55,10 +64,9 @@ while True:
                 except FileNotFoundError:
                     pass
 
-                # Unsuccessful to run program. Display Error
+            # Unsuccessful to run program. Display Error
             os.write(1, ("%s command not found.\n" % args[0]).encode())
             sys.exit(1)
 
-    else:  # Parent waits for child to finish
-        child_pid_code = os.wait()
-       # os.write(1, ('Process terminated with exit code %d' % child_pid_code).encode())
+        else:  # Parent waits for child to finish
+            child_pid_code = os.wait()
